7513185082904d1c89512ef0ab16dfbd329ec7d3
Neil Nie <yongyang.nie@gmail.com>
2020-05-15 17:54:33 -0400

    added set max speed and set starting speed
    tic motor controller. Please test @badinkajink

--- software/control_system/actuators/tic_serial.py
+++ software/control_system/actuators/tic_serial.py
@@ -5,6 +5,16 @@
 #   right serial port.
 
 import serial
+import subprocess
+
+
+def serial_port(port_name, baud_rate):
+    return serial.Serial(port_name, baud_rate, timeout=0.1, write_timeout=0.1)
+
+
+def ticcmd(*args):
+    return subprocess.check_output(['ticcmd'] + list(args))
+
 
 class TicSerial(object):
     def __init__(self, port, device_number=None):
@@ -12,10 +22,12 @@ class TicSerial(object):
         self.device_number = device_number
 
     def send_command(self, cmd, *data_bytes):
-        if self.device_number == None:
+        if self.device_number is None:
             header = [cmd]  # Compact protocol
         else:
-            header = [0xAA, device_number, cmd & 0x7F]  # Pololu protocol
+            header = [0xAA, self.device_number, cmd & 0x7F]  # Pololu protocol
+
+        print(bytes(header + list(data_bytes)))
         self.port.write(bytes(header + list(data_bytes)))
 
     # Sends the "Exit safe start" command.
@@ -23,18 +35,23 @@ class TicSerial(object):
         self.send_command(0x83)
 
     # Sets the target position.
-    #
     # For more information about what this command does, see the
     # "Set target position" command in the "Command reference" section of the
     # Tic user's guide.
     def set_target_position(self, target):
         self.send_command(0xE0,
-                          ((target >>  7) & 1) | ((target >> 14) & 2) |
+                          ((target >> 7) & 1) | ((target >> 14) & 2) |
                           ((target >> 21) & 4) | ((target >> 28) & 8),
                           target >> 0 & 0x7F,
                           target >> 8 & 0x7F,
                           target >> 16 & 0x7F,
                           target >> 24 & 0x7F)
+
+    def set_max_speed(self, speed):
+        ticcmd('--max-speed', str(speed))
+
+    def set_starting_speed(self, speed):
+        ticcmd('--starting-speed', str(speed))
 
     # Gets one or more variables from the Tic.
     def get_variables(self, offset, length):
@@ -52,3 +69,4 @@ class TicSerial(object):
         if position >= (1 << 31):
             position -= (1 << 32)
         return position
+
