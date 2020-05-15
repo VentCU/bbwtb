cfe45bb416980b23450f489eb9ade1fe598f94b1
Neil Nie <yongyang.nie@gmail.com>
2020-05-15 17:21:23 -0400

    control script boilder plate added

--- software/control_system/control_script.py
+++ software/control_system/control_script.py
@@ -7,5 +7,28 @@
 #          neil.nie@columbia.edu
 # 
 
+from .actuators.tic_serial import TicSerial, serial_port
+from .sensors.pressure_sensor import PressureSensor
+from .sensors.limit_switch import LimitSwitch
+from .sensors.rotary_encoder import RotatoryEncoder
+
+# create a motor controller object
+_port = serial_port(port_name="/dev/ttyACM0", baud_rate=9600)
+motor_controller = TicSerial(port=_port, device_number=None)
+
+# create the rotary encoder
+encoder = RotatoryEncoder()
+
+# create a pressure sensor object
+pressure_sensor = PressureSensor()
+
+# create the limit switches
+upper_switch = LimitSwitch()   # TODO determine the ports
+lower_switch = LimitSwitch()
+
+
+while True:
+
+    pass
 
 
