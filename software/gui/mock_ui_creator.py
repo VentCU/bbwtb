eeb40d7726cd92929220a04e77f634855705e195
Neil Nie <yongyang.nie@gmail.com>
2020-05-17 19:29:05 -0400

    updated mock ui, hide the butthons, added some color

--- software/gui/mock_ui_creator.py
+++ software/gui/mock_ui_creator.py
@@ -35,10 +35,12 @@ class MainWindow(QtWidgets.QMainWindow):
         flow_data = []
         volume_data = []
 
-        pen = pg.mkPen(color=(255, 0, 0))
-        self.pressure_curve = self.pressure_graph.plot(x_axis, pressure_data, pen=pen)
-        self.flow_curve = self.flow_graph.plot(x_axis, flow_data, pen=pen)
-        self.volume_curve = self.volume_graph.plot(x_axis, volume_data,pen=pen)
+        pen_red = pg.mkPen(color=(255, 0, 0), width=3)
+        pen_blue = pg.mkPen(color=(0, 0, 255), width=3)
+        pen_green = pg.mkPen(color=(100, 160, 100), width=3)
+        self.pressure_curve = self.pressure_graph.plot(x_axis, pressure_data, pen=pen_red)
+        self.flow_curve = self.flow_graph.plot(x_axis, flow_data, pen=pen_blue)
+        self.volume_curve = self.volume_graph.plot(x_axis, volume_data,pen=pen_green)
 
         self.pressure_graph.setMouseEnabled(x=False, y=False)
         self.flow_graph.setMouseEnabled(x=False, y=False)
@@ -53,10 +55,12 @@ class MainWindow(QtWidgets.QMainWindow):
         # self.flow_graph.setLabel('bottom', 'Time', 's')
         # self.volume_graph.setLabel('bottom', 'Time', 's')
 
+        # set graph labels
         self.pressure_graph.setLabel('left', 'Pressure', 'cmH2O')
         self.flow_graph.setLabel('left', 'Flow', 'L/m')
         self.volume_graph.setLabel('left', 'Time', 'L')
-
+        
+        # set the y axis range
         self.pressure_graph.setYRange(0, 50)
         self.flow_graph.setYRange(-60, 60)
         self.volume_graph.setYRange(0, 1)
@@ -64,7 +68,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.pressure_graph.setXRange(0, chunk_size)
         self.flow_graph.setXRange(0, chunk_size)
         self.volume_graph.setXRange(0, chunk_size)
-
+        
+        # hide the little A buttons
+        self.pressure_graph.hideButtons()
+        self.flow_graph.hideButtons()
+        self.volume_graph.hideButtons()
 
     def update_plots(self):
         global chunk_size, x_axis, pressure_data, flow_data, volume_data, plot_ptr
