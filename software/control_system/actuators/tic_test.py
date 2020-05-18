0c6916649ec91e0236bcf6a71cf00d0e038ee31c
Neil Nie <yongyang.nie@gmail.com>
2020-05-18 18:06:27 -0400

    updated motor controller script

---
+++ software/control_system/actuators/tic_test.py
@@ -0,0 +1,14 @@
+from tic import *
+import time
+
+if __name__ == '__main__':
+    
+    ticdev = TicDevice()
+    ticdev.open(vendor=0x1ffb, product_id=0x00CB)
+    
+    start = time.time()
+    ticdev.set_target_position(1000)
+    v = ticdev.get_variables()
+    log.debug(ticdev.variables['current_position'])
+    
+    print(time.time() - start)
--- software/control_system/pid_control_test.py
+++ software/control_system/pid_control_test.py
@@ -2,8 +2,9 @@
 
 from time import sleep
 import time
+import thread
 from pid_controller import PID
-from actuators.tic_usb import TicUSB
+from actuators.tic import *
 from sensors.rotary_encoder import RotatoryEncoder
 
 # define some global vars
@@ -16,14 +17,17 @@ def encoder_callback(value):
 
 
 # create a motor controller object
-motor_controller = TicUSB()
+ticdev = TicDevice()
+ticdev.open(vendor=0x1ffb, product_id=0x00CB)
 
 # create the rotary encoder
+try:
+    
 encoder = RotatoryEncoder(16, 18, callback=encoder_callback)
 
-pid = PID(P=100, D=10.0, I=0)
+pid = PID(P=100, D=12.0, I=0)
 
-encoder_u_limit = 150
+encoder_u_limit = 1200
 encoder_l_limit = 0
 
 pid.setpoint = encoder_u_limit
@@ -31,25 +35,26 @@ pid.setpoint = encoder_u_limit
 if __name__ == "__main__":
     
     while True:
-    
+        
+        # sleep(0.01)
         pid.update(encoder_value)
         value = pid.output * 15000.0 if pid.output < 1000000 else 100000
-        
-        motor_pose = motor_controller.get_current_position()
-        motor_controller.set_target_velocity(int(value))
+        ticdev.get_variables()
+        motor_pose = ticdev.variables['current_position']
+        ticdev.set_target_velocity(int(value))
         print("{}, {}, {}".format(int(value), encoder_value, motor_pose))
     
-        if (encoder_value == encoder_u_limit and value == 0):
-            # print("updating setpoint")
-            sleep(1.5)
-            pid.setpoint = encoder_l_limit
-        elif (encoder_value == encoder_l_limit and value == 0):
-            # print("updating setpoint")
-            sleep(1.5)
-            pid.setpoint = encoder_u_limit
+#         if (encoder_value == encoder_u_limit and value == 0):
+#             # print("updating setpoint")
+#             sleep(1.5)
+#             pid.setpoint = encoder_l_limit
+#         elif (encoder_value == encoder_l_limit and value == 0):
+#             # print("updating setpoint")
+#             sleep(1.5)
+#             pid.setpoint = encoder_u_limit
             
         
     
-motor_controller.hault_and_hold()
+ticdev.hault_and_hold()
 
 exit()
