9bf826828673711d7b997ead93b2a3764be811b7
badinkajink <williamxie00@gmail.com>
2020-05-19 16:22:16 -0400

    added contact limit switch gpio port

--- software/control_system/configs/gpio_map.py
+++ software/control_system/configs/gpio_map.py
@@ -5,7 +5,7 @@
 
 # sensors
 ABSOLUTE_SWITCH_PIN = 23
-CONTACT_SWITCH_PIN = 0  # Todo: define this
+CONTACT_SWITCH_PIN = 24  # Todo: define this
 ENCODER_A_PLUS_PIN = 16
 ENCODER_B_PLUS_PIN = 18
 PRESSURE_SENSOR_PIN = 0 # Todo: define this
