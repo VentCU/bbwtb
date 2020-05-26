7f3b285f6e35e746a6621d167c1492b3f4b072d8
Noah Silverstein <47999491+nsilverstein@users.noreply.github.com>
2020-05-26 16:53:30 -0600

    Add window stack and basic window navigation

--- software/control_system/gui/ui.py
+++ software/control_system/gui/ui.py
@@ -5,6 +5,89 @@ import sys
 from random import uniform
 
 
+class WindowStack(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(WindowStack, self).__init__(*args, **kwargs)
+
+        self.QtStack = QtWidgets.QStackedLayout()
+
+        # Instantiate windows
+        self.start = Start()
+        self.start_homing = StartHoming()
+        self.homing = Homing()
+        self.confirm_homing = ConfirmHoming()
+        self.edit_parameters = EditParameters()
+        self.confirm_parameters = ConfirmParameters()
+        self.main_window = MainWindow()
+        self.alarm_condition = AlarmCondition()
+
+        # Add windows to stack
+        self.QtStack.addWidget(self.start)
+        self.QtStack.addWidget(self.start_homing)
+        self.QtStack.addWidget(self.homing)
+        self.QtStack.addWidget(self.confirm_homing)
+        self.QtStack.addWidget(self.edit_parameters)
+        self.QtStack.addWidget(self.confirm_parameters)
+        self.QtStack.addWidget(self.main_window)
+        self.QtStack.addWidget(self.alarm_condition)
+
+        self.QtStack.setCurrentWidget(self.start)
+
+
+        # Setup basic window navigation
+
+        # start window buttons
+        self.start.start_button.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.start_homing)
+        )
+
+        # start_homing window buttons
+        self.start_homing.start_button.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.homing)
+        )
+        self.start_homing.cancel_button.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.start)
+        )
+        self.confirm_homing.rehome_button.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.homing)
+        )
+
+        # confirm_homing window buttons
+        self.confirm_homing.rehome_button.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.homing)
+        )
+        self.confirm_homing.confirm_button.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.edit_parameters)
+        )
+
+        # edit_parameters window buttons
+        self.edit_parameters.back_button.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.main_window)
+        )
+        self.edit_parameters.set_button.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.confirm_parameters)
+        )
+
+        # confirm_parameters window buttons
+        self.confirm_parameters.confirm_button.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.main_window)
+        )
+        self.confirm_parameters.cancel_button.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.edit_parameters)
+        )
+
+        # main_window window buttons
+        self.main_window.edit_parameters_button.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.edit_parameters)
+        )
+
+        # alarm_condition window hideButtons
+        self.alarm_condition.dismiss_alarm_button.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.main_window)
+        )
+
+
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
@@ -15,10 +98,7 @@ class MainWindow(QtWidgets.QMainWindow):
         pg.setConfigOption('foreground', 'k')
 
         # Load the UI Page
-        uic.loadUi('mock.ui', self)
-
-        self.edit_parameters_window = EditParameters(self)
-        self.edit_parameters_button.clicked.connect(lambda: self.edit_parameters_window.show())
+        uic.loadUi('main_window.ui', self)
 
         self.initialize_plots()
 
@@ -36,23 +116,23 @@ class MainWindow(QtWidgets.QMainWindow):
         plot_ptr = 0 # last updated x value
 
         pressure_data = []
-        flow_data = []
-        volume_data = []
+        # flow_data = []
+        # volume_data = []
 
         pen_red = pg.mkPen(color=(255, 0, 0), width=3)
-        pen_blue = pg.mkPen(color=(0, 0, 255), width=3)
-        pen_green = pg.mkPen(color=(100, 160, 100), width=3)
+        # pen_blue = pg.mkPen(color=(0, 0, 255), width=3)
+        # pen_green = pg.mkPen(color=(100, 160, 100), width=3)
         self.pressure_curve = self.pressure_graph.plot(x_axis, pressure_data, pen=pen_red)
-        self.flow_curve = self.flow_graph.plot(x_axis, flow_data, pen=pen_blue)
-        self.volume_curve = self.volume_graph.plot(x_axis, volume_data,pen=pen_green)
+        # self.flow_curve = self.flow_graph.plot(x_axis, flow_data, pen=pen_blue)
+        # self.volume_curve = self.volume_graph.plot(x_axis, volume_data,pen=pen_green)
 
         self.pressure_graph.setMouseEnabled(x=False, y=False)
-        self.flow_graph.setMouseEnabled(x=False, y=False)
-        self.volume_graph.setMouseEnabled(x=False, y=False)
+        # self.flow_graph.setMouseEnabled(x=False, y=False)
+        # self.volume_graph.setMouseEnabled(x=False, y=False)
 
         self.pressure_graph.setMenuEnabled(False)
-        self.flow_graph.setMenuEnabled(False)
-        self.volume_graph.setMenuEnabled(False)
+        # self.flow_graph.setMenuEnabled(False)
+        # self.volume_graph.setMenuEnabled(False)
 
         # Labeling the x-axis takes up too much space
         # self.pressure_graph.setLabel('bottom', 'Time', 's')
@@ -61,45 +141,45 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # set graph labels
         self.pressure_graph.setLabel('left', 'Pressure', 'cmH2O')
-        self.flow_graph.setLabel('left', 'Flow', 'L/m')
-        self.volume_graph.setLabel('left', 'Volume', 'L')
+        # self.flow_graph.setLabel('left', 'Flow', 'L/m')
+        # self.volume_graph.setLabel('left', 'Volume', 'L')
 
         # set the y axis range
         self.pressure_graph.setYRange(0, 50)
-        self.flow_graph.setYRange(-60, 60)
-        self.volume_graph.setYRange(0, 1)
+        # self.flow_graph.setYRange(-60, 60)
+        # self.volume_graph.setYRange(0, 1)
 
         self.pressure_graph.setXRange(0, chunk_size)
-        self.flow_graph.setXRange(0, chunk_size)
-        self.volume_graph.setXRange(0, chunk_size)
+        # self.flow_graph.setXRange(0, chunk_size)
+        # self.volume_graph.setXRange(0, chunk_size)
 
         # hide the little A buttons
         self.pressure_graph.hideButtons()
-        self.flow_graph.hideButtons()
-        self.volume_graph.hideButtons()
+        # self.flow_graph.hideButtons()
+        # self.volume_graph.hideButtons()
 
     def update_plots(self):
-        global chunk_size, x_axis, pressure_data, flow_data, volume_data, plot_ptr
+        global chunk_size, x_axis, pressure_data, plot_ptr #, flow_data, volume_data
 
         # Clear plot if chunk size reached
         if plot_ptr == chunk_size:
             x_axis = []
 
             pressure_data = []
-            flow_data = []
-            volume_data = []
+            # flow_data = []
+            # volume_data = []
 
             plot_ptr = 0
 
         x_axis.append(plot_ptr)
 
         pressure_data.append( uniform(0, 50) )
-        flow_data.append( uniform(-60, 60) )
-        volume_data.append( uniform(0, 1) )
+        # flow_data.append( uniform(-60, 60) )
+        # volume_data.append( uniform(0, 1) )
 
         self.pressure_curve.setData(x_axis, pressure_data)
-        self.flow_curve.setData(x_axis, flow_data)
-        self.volume_curve.setData(x_axis, volume_data)
+        # self.flow_curve.setData(x_axis, flow_data)
+        # self.volume_curve.setData(x_axis, volume_data)
 
         plot_ptr += 1
 
@@ -113,14 +193,66 @@ class EditParameters(QtWidgets.QMainWindow):
         # Load the UI Page
         uic.loadUi('edit_parameters.ui', self)
 
+class ConfirmParameters(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(ConfirmParameters, self).__init__(*args, **kwargs)
+
+        # Load the UI Page
+        uic.loadUi('confirm_parameters.ui', self)
+
+
+class Start(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(Start, self).__init__(*args, **kwargs)
+
+        # Load the UI Page
+        uic.loadUi('start.ui', self)
+
+
+class StartHoming(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(StartHoming, self).__init__(*args, **kwargs)
+
+        # Load the UI Page
+        uic.loadUi('start_homing.ui', self)
+
+
+class Homing(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(Homing, self).__init__(*args, **kwargs)
+
+        # Load the UI Page
+        uic.loadUi('homing.ui', self)
+
+
+class ConfirmHoming(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(ConfirmHoming, self).__init__(*args, **kwargs)
+
+        # Load the UI Page
+        uic.loadUi('confirm_homing.ui', self)
+
+
+class AlarmCondition(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(AlarmCondition, self).__init__(*args, **kwargs)
+
+        # Load the UI Page
+        uic.loadUi('alarm_condition.ui', self)
+
 
 class UI():
 
     def __init__(self):
         app = QtWidgets.QApplication(sys.argv)
-        main = MainWindow()
-        main.show()
-        sys.exit(app.exec_())
+        stack = WindowStack()
+        sys.exit( app.exec_() )
 
 
 if __name__ == '__main__':
