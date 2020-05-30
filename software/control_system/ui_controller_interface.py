b8911d1488e6d0325c39cc7b7c3439126dbf9190
Neil Nie <yongyang.nie@gmail.com>
2020-05-30 10:22:48 -0400

    change how lambda function is implemented in controller interface
    a python lambda can only be a single expression

--- software/control_system/ui_controller_interface.py
+++ software/control_system/ui_controller_interface.py
@@ -1,5 +1,6 @@
 
-from alarms.alarm_handler import AlarmsHandler
+from alarms.alarm_handler import AlarmHandler
+from alarms.alarms import *
 
 
 class UIControllerInterface:
@@ -8,17 +9,20 @@ class UIControllerInterface:
         self.ui = ventilator_ui
         self.controller = ventilator_controller
 
-        self.alarm_handler = AlarmsHandler(self.ui, self.controller)
+        self.alarm_handler = AlarmHandler(self.ui, self.controller)
 
         self.interface_elements()
 
     def interface_elements(self):
 
-        self.ui.stack.start.start_button.clicked.connect( lambda:
-            try:
-                self.controller.start()
-            except Alarm:
-                self.alarm_handler.over_pressure()
+        self.ui.stack.start.start_button.clicked.connect(
+            lambda: self.try_start_ventilation()
         )
 
         # TODO: connect UI elements....
+
+    def try_start_ventilation(self):
+        try:
+            self.controller.start_ventilation()
+        except Alarm as alarm:
+            self.alarm_handler.handle_alarms(alarm)
