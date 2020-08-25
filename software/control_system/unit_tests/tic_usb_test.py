6224b1fe3daa069d557b18e76dbb7e850573865a
Neil Nie <yongyang.nie@gmail.com>
2020-08-24 23:33:46 -0400

    modify tic usb test to check that motor can meet max specification speed

--- software/control_system/unit_tests/tic_usb_test.py
+++ software/control_system/unit_tests/tic_usb_test.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('/home/pi/Workspace/bbwtb/software/control_system')
 from actuators.tic_usb import *
 import time
 
@@ -7,8 +9,10 @@ if __name__ == '__main__':
     ticdev.open(vendor=0x1ffb, product_id=0x00CB)
     
     start = time.time()
-    ticdev.set_target_position(1000)
+    ticdev.set_target_position(-12800)
     v = ticdev.get_variables()
     log.debug(ticdev.variables['current_position'])
     
+    ticdev.wait_for_move_complete()
     print(time.time() - start)
+    #ticdev.set_target_position(0)
